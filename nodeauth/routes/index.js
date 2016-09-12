a83e3f39bd6897e65645aa2ba0b600148fb8e3d2
Lido Fernandez <lidofernandezburgos@gmail.com>
2016-09-12 02:37:10 +0200

    WIP

--- nodeauth/routes/index.js
+++ nodeauth/routes/index.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
+var blogDb = require('monk')('localhost/nodeblog');
 
 /* GET home page. */
 router.get('/', ensureAuthenticated, function(req, res) {
-  res.render('index', {
-    title: 'Members'
+  var blogDb = req.blogDb;
+  var posts = blogDb.get('posts');
+  posts.find({}, {}, function(error, posts) {
+    res.render('index', {
+      posts: posts
+    });
   });
 });
 
 function ensureAuthenticated(req, res, next) {
-  if(req.isAuthenticated()) {
+  if (req.isAuthenticated()) {
     return next();
   }
   res.redirect('/users/login');
